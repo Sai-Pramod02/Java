61e77cc6befe7074c8999eb1d03c32a42271b578
Sai-Pramod02 <107098747+Sai-Pramod02@users.noreply.github.com>
2022-09-03 16:38:11 +0530

    Add files via upload

---
+++ Comparing_Nums.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Comparing_Nums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number : ");
+        int user_number = sc.nextInt();
+        int given_number = 56;
+        System.out.println(given_number>user_number);
+    }
+}
