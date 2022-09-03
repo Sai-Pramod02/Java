61e8301daf39298f440c04fc1bfbecd7cab9f2d6
Sai-Pramod02 <107098747+Sai-Pramod02@users.noreply.github.com>
2022-09-03 16:38:41 +0530

    Add files via upload

---
+++ Formula.java
@@ -0,0 +1,10 @@
+public class Formula {
+    public static void main(String[] args) {
+        float v = 3;
+        float u = 4;
+        float a = 7;
+        float s = 5;
+        float formula = ((v*v)-(u*u))/(2*a*s);
+        System.out.println(formula);
+    }
+}
