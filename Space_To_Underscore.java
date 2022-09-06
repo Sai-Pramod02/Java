d64cfd0fe638fcaa2d40c88bfaaf68f586cf3052
Sai-Pramod02 <107098747+Sai-Pramod02@users.noreply.github.com>
2022-09-06 15:24:55 +0530

    Add files via upload

---
+++ Space_To_Underscore.java
@@ -0,0 +1,7 @@
+public class Space_To_Underscore {
+    public static void main(String[] args) {
+        String st = "Sai Pramod 02";
+        System.out.println("Replacing Spaces with underscores --> "+st.replace(' ','_'));
+
+    }
+}
