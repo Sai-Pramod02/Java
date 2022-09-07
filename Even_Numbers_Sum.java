967a319af84c0bb7b3d9447ddbafd3f251f95c61
Sai-Pramod02 <107098747+Sai-Pramod02@users.noreply.github.com>
2022-09-08 02:08:45 +0530

    Add files via upload

---
+++ Even_Numbers_Sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Even_Numbers_Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int p=0;
+        for(int i=0;i<n;i++){
+             p =p+ 2*i;
+
+
+            }
+        System.out.println(p);
+        }
+
+    }
+
---
+++ Sum_NumbersIn_8Table.java
@@ -0,0 +1,9 @@
+public class Sum_NumbersIn_8Table {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=1;i<=10;i++){
+            sum = sum + (i*8);
+        }
+        System.out.println(sum);
+    }
+}
