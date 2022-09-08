fedadc66e704d701c5b82c234d954c8e04c9a3e4
Sai-Pramod02 <107098747+Sai-Pramod02@users.noreply.github.com>
2022-09-08 14:58:25 +0530

    Add files via upload

---
+++ FloatNums_Sum.java
@@ -0,0 +1,16 @@
+public class FloatNums_Sum {
+    public static void main(String[] args) {
+        float sum =0f;
+        float []nums = {20.3f,20.5f,32.3f,40.4f,50.5f};
+        for(int i=0;i<nums.length;i++){
+
+             sum = sum + nums[i];
+
+        }
+        System.out.println(sum);
+
+    }
+
+
+
+}
